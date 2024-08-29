01f15219ef857b617e4061b79a671aba7078773c
season3689 <season3689@gmail.com>
2024-08-29 12:47:38 +0900

    api 리스트 불러오기

--- src/pages/components/organisms/moviesDetail.jsx
+++ src/pages/components/organisms/moviesDetail.jsx
@@ -1,59 +1,45 @@
 import TotalData from '../atoms/totalData'
 import { useEffect, useState } from 'react'
-import { useParams} from 'react-router-dom'
+import { useParams, Link} from 'react-router-dom'
 
 export default function MoviesDetails () {
-    const [movies_1, setMovies_1] = useState(null)
-    const [movies_2, setMovies_2] = useState(null)
+    const [movies, setMovies] = useState(null)
     const params = useParams()
     const moviesId = parseInt(params.id)
 
-    function TMDBApi_1p() {
-        TotalData
-            .get('/movie/top_rated?language=ko&page=1&region=KR')
-            .then((res) => {
-                setMovies_1(res.data)
-            })
-            .catch((err) => {
-                console.log(err)
-            })
+    async function TMDBApi () {
+        const requests = [
+            TotalData.get('/movie/top_rated?language=ko&page=1&region=KR'),
+            TotalData.get('/movie/top_rated?language=ko&page=2&region=KR'),
+            TotalData.get('/movie/top_rated?language=ko&page=3&region=KR'),
+            TotalData.get('/movie/top_rated?language=ko&page=4&region=KR'),
+            TotalData.get('/movie/top_rated?language=ko&page=5&region=KR'),
+        ]
+
+        const response = await Promise.all(requests)
+
+        const allData = response.flatMap(({data}) => data.results)
+
+        setMovies(allData)
     }
 
-    function TMDBApi_2p () {
-        TotalData
-            .get('/movie/top_rated?language=ko&page=2&region=KR')
-            .then((res) => {
-                setMovies_2(res.data)
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-    }
-    
+
     useEffect(() => {
-        TMDBApi_1p()
-        TMDBApi_2p()
+        TMDBApi()
     }, [])
 
-    const index_1 = movies_1 ? movies_1.results.findIndex((item) => item.id === moviesId) : ''
-    const index_2 = movies_2 ? movies_2.results.findIndex((item) => item.id === moviesId) : ''
-    const data_1 = movies_1 ? movies_1.results[index_1] : ''
-    const data_2 = movies_2 ? movies_2.results[index_2] : ''
+
+    const index = movies ? movies.findIndex((item) => item.id === moviesId) : ''
+    const data = movies ? movies[index] : ''
     
 
     return (<>
-    {data_1 ? <div style={{ margin: '0', paddingTop: '200px', textAlign: 'center', width: '50%'}}>
-        <img src={`https://image.tmdb.org/t/p/w400${data_1.poster_path}`} alt={data_1.title} style={{margin: '5px', border: '1px solid black'}}/>
-        <h2>{data_1.title}</h2>
-        <div>{data_1.overview}</div>
-        <div>popularity : {data_1.popularity}</div>
+    {data ? <div style={{ margin: '0', paddingTop: '200px', textAlign: 'center', width: '50%'}}>
+        <img src={`https://image.tmdb.org/t/p/w400${data.poster_path}`} alt={data.title} style={{margin: '5px', border: '1px solid black'}}/>
+        <h2>{data.title}</h2>
+        <div>{data.overview}</div>
+        <div>popularity : {data.popularity}</div>
     </div> : null}
-    
-    {data_2 ? <div style={{ margin: '0', paddingTop: '200px', textAlign: 'center', width: '50%'}}>
-        <img src={`https://image.tmdb.org/t/p/w400${data_2.poster_path}`} alt={data_2.title} style={{margin: '5px', border: '1px solid black'}}/>
-        <h2>{data_2.title}</h2>
-        <div>{data_2.overview}</div>
-        <div>popularity : {data_2.popularity}</div>
-    </div> : null}
+    <h2><Link to="/">메인으로</Link></h2>
     </>)
 }
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -3,57 +3,42 @@ import { useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
 
 export default function Main () {
-    const [movies_1, setMovies_1] = useState(null)
-    const [movies_2, setMovies_2] = useState(null)
 
+    const [movies, setMovies] = useState(null)
 
-    function TMDBApi_1p() {
-        TotalData
-            .get('/movie/top_rated?language=ko&page=1&region=KR')
-            .then((res) => {
-                setMovies_1(res.data)
-            })
-            .catch((err) => {
-                console.log(err)
-            })
+    async function TMDBApi () {
+        const requests = [
+            TotalData.get('/movie/top_rated?language=ko&page=1&region=KR'),
+            TotalData.get('/movie/top_rated?language=ko&page=2&region=KR'),
+            TotalData.get('/movie/top_rated?language=ko&page=3&region=KR'),
+            TotalData.get('/movie/top_rated?language=ko&page=4&region=KR'),
+            TotalData.get('/movie/top_rated?language=ko&page=5&region=KR'),
+        ]
+
+        const response = await Promise.all(requests)
+
+        const allData = response.flatMap(({data}) => data.results)
+        console.log('allData: ', allData)
+
+        setMovies(allData)
     }
 
-    function TMDBApi_2p () {
-        TotalData
-            .get('/movie/top_rated?language=ko&page=2&region=KR')
-            .then((res) => {
-                setMovies_2(res.data)
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-    }
 
     useEffect(() => {
-        TMDBApi_1p()
-        TMDBApi_2p()
+        TMDBApi()
     }, [])
 
     useEffect(() => {
-        console.log(movies_1)
-        console.log(movies_2)
-    }, [movies_1, movies_2])
+        console.log(movies)
+    }, [movies])
 
     return (<>
-    <h2 style={{paddingTop: '100px', textAlign: 'center'}}>TMDB top_rated 1페이지 영화목록</h2>
+    <h2 style={{paddingTop: '100px', textAlign: 'center'}}>TMDB top_rated 영화목록</h2>
     <div style={{display: 'flex', flexWrap: 'wrap', margin: '0', paddingTop: '100px', justifyContent: 'center'}}>
-        {movies_1 ? movies_1.results.map(({id, poster_path, title}) => (
+        {movies ? movies.map(({id, poster_path, title}) => (
             <Link key={id} to={`/${id}`}>
                 <img src={`https://image.tmdb.org/t/p/w300${poster_path}`} alt={title} style={{margin: '5px', border: '1px solid black'}}/>
-                </Link>)) : <div>없음</div>}
-        </div>
-
-    <h2 style={{paddingTop: '100px', textAlign: 'center'}}>TMDB top_rated 2페이지 영화목록</h2>
-    <div style={{display: 'flex', flexWrap: 'wrap', margin: '0', paddingTop: '100px', justifyContent: 'center'}}>
-        {movies_2 ? movies_2.results.map(({id, poster_path, title}) => (
-            <Link key={id} to={`/${id}`}>
-                <img src={`https://image.tmdb.org/t/p/w300${poster_path}`} alt={title} style={{margin: '5px', border: '1px solid black'}}/>
-                </Link>)) : <div>없음</div>}
+                </Link>)) : <div>Loading</div>}
         </div>
         </>)
 }
